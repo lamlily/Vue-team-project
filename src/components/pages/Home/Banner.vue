57ed5f57e2cccd19666a568914180ca4fc1a0043
lily <477800458@qq.com>
2018-11-20 21:37:33 +0800

    首页布局完成

--- src/components/pages/Home/Banner.vue
+++ src/components/pages/Home/Banner.vue
@@ -71,7 +71,10 @@ import Axios from 'axios'
                     var mySwiper = new Swiper ('.swiper-container', {
                     // direction: 'vertical', // 垂直切换选项
                     loop: true, // 循环模式选项
-                    
+                    autoplay: {
+                    delay: 2500,
+                    // disableOnInteraction: false,
+                  },
                     // 如果需要分页器
                     pagination: {
                       el: '.swiper-pagination',
